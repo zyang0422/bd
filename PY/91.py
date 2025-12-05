d41f78fed04827fae877a9c289803c35ae2fb938
zyang0422 <270990207@qq.com>
2025-12-05 16:50:40 +0800

    Update 91.py

--- PY/91.py
+++ PY/91.py
@@ -15,7 +15,7 @@ class Spider(Spider):
     def destroy(self):
         pass
 
-    host='https://91 -short.com'
+    host='https://91-short.com'
 
     headers = {
         'accept': 'application/json, text/plain, */*',
